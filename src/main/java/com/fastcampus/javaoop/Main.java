a785bda20e3733679609ce8e97c42ceeb17dcd7c
giraken <coffee93.coder@gmail.com>
2021-11-06 20:37:12 +0900

    BubbleSort Main class Call Test

--- src/main/java/com/fastcampus/javaoop/Main.java
+++ src/main/java/com/fastcampus/javaoop/Main.java
@@ -1,7 +1,13 @@
 package com.fastcampus.javaoop;
 
+import com.fastcampus.javaoop.logic.BubbleSort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello World");
+
+        BubbleSort<String> sort = new BubbleSort<>();
+        System.out.println(sort.sort(Arrays.asList(args)));
     }
 }
--- src/test/java/com/fastcampus/javaoop/MainTest.java
+++ src/test/java/com/fastcampus/javaoop/MainTest.java
@@ -2,13 +2,16 @@ package com.fastcampus.javaoop;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MainTest {
 
     @Test
     void main() {
-        Main.main(null);
+        //Given
+        String[] args = {"3", "1", "2"};
+
+        // When & Then
+        Main.main(args);
+
     }
 
 }
